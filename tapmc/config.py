588980cfa7f27ee2e01f75c62a8eff119c7228ea
cjwang <cjwang0318@gmail.com>
2023-10-02 15:30:13 +0800

    update 1.0

--- tapmc/config.py
+++ tapmc/config.py
@@ -1,4 +1,8 @@
+from ray import tune
+from ray.tune.search.hyperopt import HyperOptSearch
+
 # 模型設定檔
+
 config_patchtst = {
     'input_size': tune.choice([5, 7, 14]),
     'max_steps': 500,
@@ -18,27 +22,30 @@ config_patchtst = {
 }
 
 config_lstm = {
-    'input_size':tune.choice([5, 7, 14]),
-    'max_steps':500,
-    'val_check_steps':tune.choice([20, 30]),
-    'early_stop_patience_steps':tune.choice([3]),
-    'encoder_n_layers':tune.choice([3, 5, 7]),
-    'encoder_dropout':tune.choice([0.1, 0.2]),
-    'decoder_layers':tune.choice([1, 2, 3, 4]),
-    'scaler_type':tune.choice(['standard']),
-    'learning_rate':tune.loguniform(1e-4, 1e-2),
-    'batch_size':tune.choice([256, 512, 1024]),
-    'encoder_hidden_size':tune.choice([128, 256, 512]),
-    'decoder_hidden_size':tune.choice([128, 256, 512])
-    }
-    
+    'input_size': tune.choice([5, 7, 14]),
+    'max_steps': 500,
+    'val_check_steps': tune.choice([20, 30]),
+    'early_stop_patience_steps': tune.choice([3]),
+    'encoder_n_layers': tune.choice([3, 5, 7]),
+    'encoder_dropout': tune.choice([0.1, 0.2]),
+    'decoder_layers': tune.choice([1, 2, 3, 4]),
+    'scaler_type': tune.choice(['standard']),
+    'learning_rate': tune.loguniform(1e-4, 1e-2),
+    'batch_size': tune.choice([256, 512, 1024]),
+    'encoder_hidden_size': tune.choice([128, 256, 512]),
+    'decoder_hidden_size': tune.choice([128, 256, 512])
+}
+
 config_nhits = {
     "max_steps": 500,  # Number of SGD steps
     "input_size": tune.choice([5, 7, 14]),  # Size of input window
     "learning_rate": tune.loguniform(1e-5, 1e-1),  # Initial Learning rate
-    "n_pool_kernel_size": tune.choice([[2, 2, 2], [16, 8, 1], [64, 16, 1]]),  # MaxPool's Kernelsize
-    "n_freq_downsample": tune.choice([[168, 24, 1], [24, 12, 1], [1, 1, 1]]),  # Interpolation expressivity ratios
-    "val_check_steps": tune.choice([20, 30]),  # Compute validation every 50 steps
+    # MaxPool's Kernelsize
+    "n_pool_kernel_size": tune.choice([[2, 2, 2], [16, 8, 1], [64, 16, 1]]),
+    # Interpolation expressivity ratios
+    "n_freq_downsample": tune.choice([[168, 24, 1], [24, 12, 1], [1, 1, 1]]),
+    # Compute validation every 50 steps
+    "val_check_steps": tune.choice([20, 30]),
     "dropout_prob_theta": tune.choice([0.2, 0.3, 0.4]),
     "batch_size": tune.choice([32, 64, 128, 256, 512]),
     "windows_batch_size": tune.choice([128, 256, 512])  # Random seed
--- tapmc/tapmc.py
+++ tapmc/tapmc.py
@@ -1,17 +1,21 @@
-import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
-import os
-from neuralforecast.core import NeuralForecast
-from neuralforecast.models import PatchTST, LSTM
-from neuralforecast.auto import AutoLSTM, AutoPatchTST, AutoNHITS
-from neuralforecast.losses.pytorch import MQLoss, MAPE, HuberMQLoss, MAE
+import pandas as pd
 from neuralforecast.losses.numpy import mape, rmse
-from ray import tune
-# from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
-from ray.tune.search.hyperopt import HyperOptSearch
+from neuralforecast.losses.pytorch import MQLoss, MAPE, HuberMQLoss, MAE
+from neuralforecast.auto import AutoLSTM, AutoPatchTST, AutoNHITS
+from neuralforecast.models import PatchTST, LSTM
+from neuralforecast.core import NeuralForecast
 import json
 import config as cf
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+# from ray import tune
+# from ray.tune.search.hyperopt import HyperOptSearch
+
+# import matplotlib.pyplot as plt
+# from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
+
 
 def insert_ds(df):
     uids = df['unique_id'].unique()  # Select ids
@@ -20,40 +24,32 @@ def insert_ds(df):
         df_temp = df.query('unique_id in @id').reset_index(drop=True)
         start = 1
         df_temp.insert(2, 'ds', range(start, start + df_temp.shape[0]))
-        #print(Y_df.shape)
-        #print(Y_df.head(20))
+        # print(Y_df.shape)
+        # print(Y_df.head(20))
         df_return = pd.concat([df_return, df_temp], axis=0, ignore_index=True)
-    #print(df_return.tail())
+    # print(df_return.tail())
     return df_return
 
-if __name__ == '__main__':
-    # ds使用連續自然數
-    #df = pd.read_csv('./dataset/NTU/test.csv', header=None, names=["unique_id", "datetime", "y"])
-    #df = insert_ds(df)
-    #df = df.drop('datetime', axis=1) # where 1 is the axis number (0 for rows and 1 for columns.)
-    
-    # 使用資料原本格式
-    df = pd.read_csv('./dataset/NTU/test_all.txt', header=None, names=["unique_id", "ds", "y"])
-    # 修改成 pandas的datetime格式
-    #df['ds'] = pd.to_datetime(df['ds'])  # transform to datetime formate
-    
-    #print(df.head())
 
-    #預測時間參數
-    horizon = 3
-    # cross_validation 參數
-    n_windows = 1
-
-
-    
-
+def model_training(horizon, model, n_windows, num_samples=2):
     # Utilizing Auto-models for parameters tuning by random grid search, and choose the best parameters by cross validation
-
-    models = [
-        AutoLSTM(h=horizon, config=cf.config_lstm, loss=MQLoss(), num_samples=20),
-        #AutoPatchTST(h=horizon, config=config_patchtst, loss=MQLoss(), num_samples=2),
-        #AutoNHITS(h=horizon, config=config_nhits, loss=MQLoss(), num_samples=2)
-    ]
+    if model == "LSTM":
+        models = [AutoLSTM(h=horizon, config=cf.config_lstm,
+                           loss=MQLoss(), num_samples=num_samples)]
+    elif model == "PatchTST":
+        models = [AutoPatchTST(h=horizon, config=cf.config_patchtst,
+                               loss=MQLoss(), num_samples=num_samples)]
+    elif model == "NHITS":
+        models = [AutoNHITS(h=horizon, config=cf.config_nhits,
+                            loss=MQLoss(), num_samples=num_samples)]
+    else:
+        print("NO model is selected...")
+        # models = [
+        #     AutoLSTM(h=horizon, config=cf.config_lstm,
+        #              loss=MQLoss(), num_samples=num_samples),
+        #     AutoPatchTST(h=horizon, config=config_patchtst,
+        #                  loss=MQLoss(), num_samples=num_samples),
+        #     AutoNHITS(h=horizon, config=config_nhits, loss=MQLoss(), num_samples=num_samples)]
 
     nf = NeuralForecast(models=models, freq='H')
     """
@@ -63,38 +59,78 @@ if __name__ == '__main__':
     
     """
     Y_hat_df = nf.cross_validation(df=df, n_windows=n_windows)
-    
+
     # 儲存模型
     # [0] represents the first index of the modes_list, it's AutoLSTM
-    nf.save(path='./checkpoints/AutoLSTM_day3', model_index=[0], overwrite=True, save_dataset=True)
-    # [0] represents the best LSTM parameters by previous training 
-    LSTM_best_config=nf.models[0].results.get_best_result().config
-    print(f"LSTM_best_config=\n{LSTM_best_config}")
-    with open('./checkpoints/AutoLSTM_day3/best_LSTM_config.json', 'w') as f:
-        f.write(str(LSTM_best_config))
-    
-    #顯示結果
-    Y_hat_df.columns = Y_hat_df.columns.str.replace('-median', '')
-    print(Y_hat_df)
-    
-    # 儲存預測結果
-    Y_hat_df.to_csv("result.csv",index=False)
-    
-    # 輸出Evaluation結果
-    modle_list=["LSTM"]
-    MAPE_result=[]
-    RMSE_result=[]
-    MAPE=round(mape(Y_hat_df['y'], Y_hat_df['AutoLSTM']),4)
-    MAPE_result.append(MAPE)
-    RMSE=round(rmse(Y_hat_df['y'], Y_hat_df['AutoLSTM']),4)
-    RMSE_result.append(RMSE)
-    evaluation_result={
-        "Model":modle_list,
-        "MAPE":MAPE_result,
-        "RMSE":RMSE_result
-         }
-    result_df=pd.DataFrame(evaluation_result)
+    nf.save(path=f'./checkpoints/{model}_day{horizon}',
+            model_index=[0], overwrite=True, save_dataset=True)
+    # [0] represents the best LSTM parameters by previous training
+    best_config = nf.models[0].results.get_best_result().config
+    print(f"best_config=\n{best_config}")
+    with open(f'./checkpoints/{model}_day{horizon}/best_{model}_config.json', 'w') as f:
+        f.write(str(best_config))
+    return Y_hat_df
+
+
+def model_evaluation(trained_model):
+    path = "./result"
+    model_list = []
+    MAPE_result = []
+    RMSE_result = []
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    for model in trained_model:
+        print(f"Evaluating {model} model")
+        Y_hat_df = trained_model.get(model)
+        # 顯示結果
+        Y_hat_df.columns = Y_hat_df.columns.str.replace('-median', '')
+        print(Y_hat_df)
+        # 儲存預測結果
+        Y_hat_df.to_csv(f"{path}/{model}_result.csv", index=False)
+        # 輸出Evaluation結果
+        model_list.append(model)
+        tag = f"Auto{model}"
+        MAPE = round(mape(Y_hat_df['y'], Y_hat_df[tag]), 4)
+        MAPE_result.append(MAPE)
+        RMSE = round(rmse(Y_hat_df['y'], Y_hat_df[tag]), 4)
+        RMSE_result.append(RMSE)
+    evaluation_result = {
+        "Model": model_list,
+        "MAPE": MAPE_result,
+        "RMSE": RMSE_result
+    }
+    result_df = pd.DataFrame(evaluation_result)
     print(result_df)
-    result_df.to_csv("evaluation.csv",index=False)
+    result_df.to_csv(f"{path}/evaluation.csv", index=False)
 
 
+if __name__ == '__main__':
+    # ds使用datetime格式
+    # df = pd.read_csv('./dataset/NTU/test.csv', header=None, names=["unique_id", "datetime", "y"])
+    # df = df.drop('datetime', axis=1) # where 1 is the axis number (0 for rows and 1 for columns.)
+
+    # ds使用datetime格式 -> 連續自然數格式
+    # df = pd.read_csv('./dataset/NTU/test.csv', header=None, names=["unique_id", "datetime", "y"])
+    # df = insert_ds(df)
+
+    # ds使用連續自然數
+    df = pd.read_csv('./dataset/NTU/test/test_all.txt',
+                     header=None, names=["unique_id", "ds", "y"])
+    # print(df.head())
+
+    # 預測模型
+    modelList = ["LSTM"]
+
+    # 預測時間參數
+    horizon = 3
+    # cross_validation 參數
+    n_windows = 1
+
+    # 訓練模型
+    trained_model = {}
+    for model in modelList:
+        Y_hat_df = model_training(horizon, model, n_windows, 2)
+        trained_model[model] = Y_hat_df
+
+    # 評估模型
+    model_evaluation(trained_model)
