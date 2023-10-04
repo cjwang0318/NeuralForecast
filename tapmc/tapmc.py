13ada6c9d2884829dc628c9e0c02e07d29fd2e66
cjwang <cjwang0318@gmail.com>
2023-10-04 08:29:02 +0800

    Change Formate

--- tapmc/tapmc.py
+++ tapmc/tapmc.py
@@ -18,32 +18,48 @@ import config as cf
 # import matplotlib.pyplot as plt
 # from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
 
+
 def insert_ds(df):
-    uids = df['unique_id'].unique()  # Select ids
+    uids = df["unique_id"].unique()  # Select ids
     df_return = pd.DataFrame()
     for id in uids:
-        df_temp = df.query('unique_id in @id').reset_index(drop=True)
+        df_temp = df.query("unique_id in @id").reset_index(drop=True)
         start = 1
-        df_temp.insert(2, 'ds', range(start, start + df_temp.shape[0]))
+        df_temp.insert(2, "ds", range(start, start + df_temp.shape[0]))
         # print(Y_df.shape)
         # print(Y_df.head(20))
         df_return = pd.concat([df_return, df_temp], axis=0, ignore_index=True)
-    df_return.drop('datetime', inplace=True, axis=1)
-    #print(df_return.tail())
+    df_return.drop("datetime", inplace=True, axis=1)
+    # print(df_return.tail())
     return df_return
 
 
 def model_training(df, horizon, model, n_windows, num_samples, checkpointsPath):
     # Utilizing Auto-models for parameters tuning by random grid search, and choose the best parameters by cross validation
     if model == "LSTM":
-        models = [AutoLSTM(h=horizon, config=cf.config_lstm,
-                           loss=MQLoss(), num_samples=num_samples)]
+        models = [
+            AutoLSTM(
+                h=horizon, config=cf.config_lstm, loss=MQLoss(), num_samples=num_samples
+            )
+        ]
     elif model == "PatchTST":
-        models = [AutoPatchTST(h=horizon, config=cf.config_patchtst,
-                               loss=MQLoss(), num_samples=num_samples)]
+        models = [
+            AutoPatchTST(
+                h=horizon,
+                config=cf.config_patchtst,
+                loss=MQLoss(),
+                num_samples=num_samples,
+            )
+        ]
     elif model == "NHITS":
-        models = [AutoNHITS(h=horizon, config=cf.config_nhits,
-                            loss=MQLoss(), num_samples=num_samples)]
+        models = [
+            AutoNHITS(
+                h=horizon,
+                config=cf.config_nhits,
+                loss=MQLoss(),
+                num_samples=num_samples,
+            )
+        ]
     else:
         print("NO model is selected...")
         # models = [
@@ -53,7 +69,7 @@ def model_training(df, horizon, model, n_windows, num_samples, checkpointsPath):
         #                  loss=MQLoss(), num_samples=num_samples),
         #     AutoNHITS(h=horizon, config=config_nhits, loss=MQLoss(), num_samples=num_samples)]
 
-    nf = NeuralForecast(models=models, freq='H')
+    nf = NeuralForecast(models=models, freq="H")
     """
     nf.fit(df=df)
     Y_hat_df = nf.predict()
@@ -64,12 +80,18 @@ def model_training(df, horizon, model, n_windows, num_samples, checkpointsPath):
 
     # 儲存模型
     # [0] represents the first index of the modes_list, it's AutoLSTM
-    nf.save(path=f'{checkpointsPath}/{model}_day{horizon}',
-            model_index=[0], overwrite=True, save_dataset=True)
+    nf.save(
+        path=f"{checkpointsPath}/{model}_day{horizon}",
+        model_index=[0],
+        overwrite=True,
+        save_dataset=True,
+    )
     # [0] represents the best LSTM parameters by previous training
     best_config = nf.models[0].results.get_best_result().config
     print(f"best_config=\n{best_config}")
-    with open(f'{checkpointsPath}/{model}_day{horizon}/best_{model}_config.json', 'w') as f:
+    with open(
+        f"{checkpointsPath}/{model}_day{horizon}/best_{model}_config.json", "w"
+    ) as f:
         f.write(str(best_config))
     return Y_hat_df
 
@@ -85,35 +107,41 @@ def model_evaluation(trained_model, trained_model_time, horizon, resultPath):
         Y_hat_df = trained_model.get(model)
         Train_time = trained_model_time.get(model)
         # 顯示結果
-        Y_hat_df.columns = Y_hat_df.columns.str.replace('-median', '')
+        Y_hat_df.columns = Y_hat_df.columns.str.replace("-median", "")
         print(Y_hat_df)
         # 儲存預測結果
         Y_hat_df.to_csv(f"{path}/{model}_result_day{horizon}.csv", index=False)
         # 輸出Evaluation結果
         model_list.append(model)
         tag = f"Auto{model}"
-        MAPE = round(mape(Y_hat_df['y'], Y_hat_df[tag]), 4)
+        MAPE = round(mape(Y_hat_df["y"], Y_hat_df[tag]), 4)
         MAPE_result.append(MAPE)
-        RMSE = round(rmse(Y_hat_df['y'], Y_hat_df[tag]), 4)
+        RMSE = round(rmse(Y_hat_df["y"], Y_hat_df[tag]), 4)
         RMSE_result.append(RMSE)
-        Training_time.append(round(Train_time,2))
+        Training_time.append(round(Train_time, 2))
     evaluation_result = {
         "Model": model_list,
         "MAPE": MAPE_result,
         "RMSE": RMSE_result,
-        "Training_Time": Training_time
+        "Training_Time": Training_time,
     }
     result_df = pd.DataFrame(evaluation_result)
     print(result_df)
     result_df.to_csv(f"{path}/evaluation_day{horizon}.csv", index=False)
 
 
-if __name__ == '__main__':
-    filePath_List=["./dataset/NTU/181697_Top70.csv","./dataset/NTU/566768_Top70.csv","./dataset/NTU/758085_Top70.csv","./dataset/NTU/849886_Top70.csv","./dataset/NTU/890285_Top70.csv"]
-    #filePath_List=["./dataset/NTU/test/test.csv","./dataset/NTU/test/test_all.txt"]
-    #filePath='./dataset/NTU/181697_Top70.csv'
-    #filePath='./dataset/NTU/test/test_all.txt'
-    
+if __name__ == "__main__":
+    filePath_List = [
+        "./dataset/NTU/181697_All.csv",
+        "./dataset/NTU/566768_All.csv",
+        "./dataset/NTU/758085_All.csv",
+        "./dataset/NTU/849886_All.csv",
+        "./dataset/NTU/890285_All.csv",
+    ]
+    # filePath_List=["./dataset/NTU/test/test.csv","./dataset/NTU/test/test_all.txt"]
+    # filePath='./dataset/NTU/181697_Top70.csv'
+    # filePath='./dataset/NTU/test/test_all.txt'
+
     # 預測模型
     modelList = ["LSTM", "PatchTST", "NHITS"]
     # 預測時間參數
@@ -123,13 +151,15 @@ if __name__ == '__main__':
     num_samples = 25
 
     for filePath in filePath_List:
-        for horizon in range(1, horizon_for_testing+1):
+        for horizon in range(1, horizon_for_testing + 1):
             # ds使用datetime格式
             # df = pd.read_csv(filePath, header=None, names=["unique_id", "datetime", "y"])
             # df = df.drop('datetime', axis=1) # where 1 is the axis number (0 for rows and 1 for columns.)
 
             # ds使用datetime格式 -> 連續自然數格式
-            df = pd.read_csv(filePath, header=None, names=["unique_id", "datetime", "y"])
+            df = pd.read_csv(
+                filePath, header=None, names=["unique_id", "datetime", "y"]
+            )
             df = insert_ds(df)
 
             # ds使用連續自然數
@@ -137,18 +167,18 @@ if __name__ == '__main__':
             #                  header=None, names=["unique_id", "ds", "y"])
             # print(df.head())
 
-            #獲取檔案名稱
+            # 獲取檔案名稱
             # file name with extension
             file_name = os.path.basename(filePath)
             # file name without extension
-            file_name=os.path.splitext(file_name)[0]
+            file_name = os.path.splitext(file_name)[0]
             print(f"Training: {file_name}")
-            
-            #建立存放結果的資料夾
-            resultPath=f"./result/{file_name}"
+
+            # 建立存放結果的資料夾
+            resultPath = f"./result/{file_name}"
             if not os.path.isdir(resultPath):
                 os.mkdir(resultPath)
-            checkpointsPath=f"./checkpoints/{file_name}"
+            checkpointsPath = f"./checkpoints/{file_name}"
             if not os.path.isdir(checkpointsPath):
                 os.mkdir(checkpointsPath)
 
@@ -157,7 +187,9 @@ if __name__ == '__main__':
             trained_model_time = {}
             for model in modelList:
                 start_time = time.time()
-                Y_hat_df = model_training(df, horizon, model, n_windows, num_samples, checkpointsPath)
+                Y_hat_df = model_training(
+                    df, horizon, model, n_windows, num_samples, checkpointsPath
+                )
                 end_time = time.time()
                 execution_time = end_time - start_time
                 trained_model[model] = Y_hat_df
